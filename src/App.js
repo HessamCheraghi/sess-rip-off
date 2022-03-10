d1e9bba54009e2b1d0e3b3118106cb1d6e3d62a1
HessamCheraghi <hessam.ch.rose@gmail.com>
2022-03-10 22:45:03 +0330

    making base files and creating app layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import RTL from "./components/RTL";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+//components
+import Layout from "./components/Layout";
+
+const theme = createTheme({
+  direction: "rtl",
+  typography: {
+    fontFamily: "'Vazirmatn', 'Roboto',sans-serif",
+  },
+});
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RTL>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Layout />
+      </ThemeProvider>
+    </RTL>
   );
 }
 
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,41 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+export default function Layout() {
+  //TODO make different layouts based on props
+  return (
+    <>
+      <Container maxWidth="xl">
+        <Box sx={{ bgcolor: "#cfe8fc", height: "25vh", marginBottom: "16px" }}>
+          <Typography>هدر</Typography>
+        </Box>
+        <Grid
+          container
+          spacing={2}
+          direction={{ xs: "column-reverse", lg: "row" }} // necessary for mobile responsiveness
+        >
+          <Grid item xs={12} lg={3}>
+            <Box sx={{ bgcolor: "#cfe8fc", height: "25vh" }}>
+              <Typography>ساید بار سمت راست</Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} lg={6}>
+            <Box sx={{ bgcolor: "#cfe8fc", height: "25vh" }}>
+              <Typography>وسط </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} lg={3}>
+            <Box sx={{ bgcolor: "#cfe8fc", height: "25vh" }}>
+              <Typography>پروفایل</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "25vh", marginTop: "16px" }}>
+          <Typography>فوتر</Typography>
+        </Box>
+      </Container>
+    </>
+  );
+}
